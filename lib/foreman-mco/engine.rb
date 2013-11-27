66ca36eb7bcee5d2f2caa8e2f930d04915894b3a
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-11-27 14:15:51 +0000

    updated to use Foreman::Plugin api

--- lib/foreman-mco/engine.rb
+++ lib/foreman-mco/engine.rb
@@ -15,13 +15,23 @@ module ForemanMco
       require_dependency File.expand_path("../../../app/models/setting/mco.rb", __FILE__) if (Setting.table_exists? rescue(false))
     end
 
+    initializer 'foreman_mco.register_plugin', :after=> :finisher_hook do |app|
+      Foreman::Plugin.register :foreman_discovery do
+        requires_foreman '> 1.3'
+
+        menu :top_menu, :mco_history, :url_hash => {:controller=> :command_histories, :action=>:index},
+          :caption=> N_('MCollective Command History'),
+          :parent => :monitor_menu,
+          :after=>:audits
+        end
+    end
+
     initializer "foreman_mco.load_app_instance_data" do |app|
       app.config.paths['db/migrate'] += ForemanMco::Engine.paths['db/migrate'].existent
     end
 
     config.to_prepare do
       ::HostsHelper.send :include, ForemanMco::HostsHelper
-      ::Menu::Loader.send :include, ForemanMco::MenuHelper
     end
   end
 
