1018acd84965edd2aad880da3e2a656c1f8d1726
Sam Kottler <shk@redhat.com>
2013-12-09 15:20:43 -0500

    Add discovery-related tables

---
+++ db/migrate/20131209192923_add_discovered_agents.rb
@@ -0,0 +1,20 @@
+class AddDiscoveredAgents < ActiveRecord::Migration
+  def up
+    create_table :foreman_mco_discovered_agents do |t|
+      t.column :id, :integer, :null => false
+      t.column :name, :string, :null => false
+      t.column :type, :string, :null => false
+    end
+
+    create_table :foreman_mco_discovered_agent_fields do |t|
+      t.column :id, :integer, :null => false
+      t.column :name, :string, :null => false
+      t.column :discovered_agents_id, :integer, :null => false
+    end
+  end
+
+  def down
+    drop_table :discovered_agents
+    drop_table :discovered_agent_fields
+  end
+end
